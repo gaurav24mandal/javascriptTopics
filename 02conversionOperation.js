60702a0d78963f7ca2ef25e57b10161caff898c3
gaurav mandal <gauravmandal97255@gmail.com>
2023-09-25 11:10:07 +0530

    operators

--- 02conversionOperation.js
+++ 02conversionOperation.js
@@ -21,3 +21,33 @@ console.log(typeof b); // op : number
 let c  = "hu";
 let ans = Number(c)
 console.log(ans); // op :  NaN
+
+// oprators 
+// + - * / % 
+
+let str1 = "hi"
+let str2 = " gaurav"
+console.log(str1+str2);
+// output --> hi gaurav
+
+let num1  = '1' ;
+let num2  = '2'
+console.log(num1 + num2);
+//output --> 12
+
+console.log(1 + 1 +'2');
+// op --> 22 
+
+console.log('2'+1 + 1);
+// op --> 211
+
+
+
+// prefix  and  postfix oprerators
+
+let x = 3 ;
+let y = x++;
+console.log(`x is ${x} and y is ${y}`);
+let x1 = 3 ;
+let y2 = ++x1;
+console.log(`x is ${x1} and y is ${y2}`);
