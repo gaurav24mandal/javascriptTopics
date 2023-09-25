299b9e52873450fcf178e14c4b5284d7219420a6
gaurav mandal <gauravmandal97255@gmail.com>
2023-09-25 09:54:44 +0530

    datatypes

---
+++ 01dataTYpes.js
@@ -0,0 +1,38 @@
+// "use strict"  treat all js code as  newer version
+// data  types
+
+
+ // number 
+let age = 9
+
+ // string 
+ let name = "gaurav"
+
+ // boolean
+ let result = false ;
+
+ // null is a standalone data type
+ let container  = null ;
+// null indicates  that the variable is  empty 
+
+// undefined this  data represnts a condition when a variable   is not define 
+  let salary ;
+
+
+  //object
+let student = {
+    name : '',
+    age : 4,
+    class : 4,
+}
+// here student is a  object , object  itself  contain many data type
+
+// typeof  : IT IS KEYWORD USED TO IDENTIFY THE  TYPE OF A VARIABLE
+
+console.log(typeof student); // output : object 
+console.log(typeof undefined); // op : undefined i.e  it s  datatype
+console.log(null);  // op : null and object 
+
+
+
+
